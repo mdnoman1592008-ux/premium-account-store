db41b5cb8cfe002d52560c76a99ac5e064c32675
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 21:39:05 +0600

    Create page.tsx

---
+++ frontend/src/app/privacy-policy/page.tsx
@@ -0,0 +1,45 @@
+export default function PrivacyPolicyPage() {
+
+  return (
+    <div
+      style={{
+        padding:"50px",
+        maxWidth:"900px",
+        margin:"auto"
+      }}
+    >
+
+      <h1>
+        Privacy Policy
+      </h1>
+
+      <p
+        style={{
+          marginTop:"20px"
+        }}
+      >
+        Premium Account Store
+        collects only the
+        information required
+        to process orders,
+        verify payments and
+        provide customer
+        support.
+      </p>
+
+      <p>
+        We do not sell user
+        information to third
+        parties.
+      </p>
+
+      <p>
+        Payment screenshots,
+        transaction IDs and
+        account information
+        are stored securely.
+      </p>
+
+    </div>
+  );
+}
