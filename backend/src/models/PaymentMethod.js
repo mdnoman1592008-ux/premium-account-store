4f8ef262213dd590e33fdc6f7b690d00598d16ad
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 01:48:26 +0600

    Update PaymentMethod.js

--- backend/src/models/PaymentMethod.js
+++ backend/src/models/PaymentMethod.js
@@ -1,45 +1,39 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db");
+const { DataTypes } =
+require("sequelize");
 
-const PaymentMethod = sequelize.define(
+const { sequelize } =
+require("../config/db");
+
+const PaymentMethod =
+sequelize.define(
   "PaymentMethod",
   {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+
+    id:{
+      type:DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true
     },
 
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
+    name:{
+      type:DataTypes.STRING
     },
 
-    logo: {
-      type: DataTypes.STRING
+    accountNumber:{
+      type:DataTypes.STRING
     },
 
-    accountNumber: {
-      type: DataTypes.STRING,
-      allowNull: false
+    accountType:{
+      type:DataTypes.STRING
     },
 
-    accountType: {
-      type: DataTypes.STRING,
-      defaultValue: "Personal"
-    },
-
-    status: {
-      type: DataTypes.ENUM(
-        "active",
-        "inactive"
-      ),
-      defaultValue: "active"
+    active:{
+      type:DataTypes.BOOLEAN,
+      defaultValue:true
     }
-  },
-  {
-    timestamps: true
+
   }
 );
 
-module.exports = PaymentMethod;
+module.exports =
+PaymentMethod;
