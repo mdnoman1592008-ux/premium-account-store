5cf8c5efefa870390b410f9988a6354972bb4150
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 20:42:15 +0600

    Create Admin.js

---
+++ backend/src/models/Admin.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Admin = sequelize.define(
+  "Admin",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    role: {
+      type: DataTypes.ENUM("super_admin", "admin"),
+      defaultValue: "admin"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = Admin;
