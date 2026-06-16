7c2329af110f80e8cd7986690e6990d496581b80
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 00:27:48 +0600

    Create HomePage.tsx

---
+++ frontend/src/components/HomePage.tsx
@@ -0,0 +1,36 @@
+import HeroSection from "./HeroSection";
+import FeaturedApps from "./FeaturedApps";
+import StatsSection from "./StatsSection";
+import WhyChooseUs from "./WhyChooseUs";
+import LatestOrders from "./LatestOrders";
+import Testimonials from "./Testimonials";
+import PaymentMethodsSection from "./PaymentMethodsSection";
+import FAQ from "./FAQ";
+import Newsletter from "./Newsletter";
+
+export default function HomePage() {
+
+  return (
+    <>
+
+      <HeroSection />
+
+      <FeaturedApps />
+
+      <StatsSection />
+
+      <WhyChooseUs />
+
+      <LatestOrders />
+
+      <Testimonials />
+
+      <PaymentMethodsSection />
+
+      <FAQ />
+
+      <Newsletter />
+
+    </>
+  );
+}
