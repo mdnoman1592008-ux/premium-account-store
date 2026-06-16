b221941a8193fd2f6a7fa3decd7caa8edf473fc2
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 00:31:07 +0600

    Create page.tsx

---
+++ frontend/src/app/admin/login/page.tsx
@@ -0,0 +1,17 @@
+import AdminLogin from
+"../../../components/AdminLogin";
+
+export default function AdminLoginPage() {
+
+  return (
+    <div
+      style={{
+        padding:"100px 20px"
+      }}
+    >
+
+      <AdminLogin />
+
+    </div>
+  );
+}
