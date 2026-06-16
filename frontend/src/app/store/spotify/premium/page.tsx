33b30802cde849ec8b06dae7072b0fcc391422f6
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 00:17:09 +0600

    Create page.tsx

---
+++ frontend/src/app/store/spotify/premium/page.tsx
@@ -0,0 +1,46 @@
+import DurationCard from
+"../../../../components/DurationCard";
+
+export default function SpotifyPremiumPage() {
+
+  return (
+    <div style={{padding:"50px"}}>
+
+      <h1>Spotify Premium</h1>
+
+      <div
+        className="grid"
+        style={{marginTop:"30px"}}
+      >
+
+        <DurationCard
+          title="3 Months"
+          regularPrice="৳299"
+          finalPrice="৳199"
+          discount="33%"
+          save="৳100"
+        />
+
+        <DurationCard
+          title="6 Months"
+          regularPrice="৳599"
+          finalPrice="৳349"
+          discount="42%"
+          save="৳250"
+          badge="Popular"
+        />
+
+        <DurationCard
+          title="12 Months"
+          regularPrice="৳1199"
+          finalPrice="৳599"
+          discount="50%"
+          save="৳600"
+          badge="Best Value"
+        />
+
+      </div>
+
+    </div>
+  );
+}
