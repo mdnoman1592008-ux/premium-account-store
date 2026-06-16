6d6005ff39787fed48cf285472086698c0e9e63f
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 02:08:00 +0600

    Update page.tsx

--- frontend/src/app/my-accounts/page.tsx
+++ frontend/src/app/my-accounts/page.tsx
@@ -1,1 +1,46 @@
+import DeliveredAccountCard
+from
+"../../components/DeliveredAccountCard";
 
+export default function
+MyAccountsPage(){
+
+  return (
+
+    <div
+      style={{
+        padding:"50px"
+      }}
+    >
+
+      <h1>
+        My Accounts
+      </h1>
+
+      <div
+        className="grid"
+        style={{
+          marginTop:"30px"
+        }}
+      >
+
+        <DeliveredAccountCard
+
+          appName="ChatGPT"
+
+          planName="Plus"
+
+          email=
+          "user@gmail.com"
+
+          password=
+          "password123"
+
+        />
+
+      </div>
+
+    </div>
+
+  );
+}
