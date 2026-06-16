0d13395fdcdf6d7fd429460de3915e9efa15c790
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 21:41:14 +0600

    Create page.tsx

---
+++ frontend/src/app/refund-policy/page.tsx
@@ -0,0 +1,41 @@
+export default function RefundPolicyPage() {
+
+  return (
+    <div
+      style={{
+        padding:"50px",
+        maxWidth:"900px",
+        margin:"auto"
+      }}
+    >
+
+      <h1>
+        Refund Policy
+      </h1>
+
+      <p
+        style={{
+          marginTop:"20px"
+        }}
+      >
+        Refund requests are
+        reviewed individually.
+      </p>
+
+      <p>
+        If payment is verified
+        but account delivery
+        fails, a replacement
+        or refund may be
+        provided.
+      </p>
+
+      <p>
+        Successfully delivered
+        accounts are generally
+        non-refundable.
+      </p>
+
+    </div>
+  );
+}
