ff49f338a235cbba36307a547cf546c39f1a4072
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 01:18:57 +0600

    Update payment.controller.js

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,127 +1,50 @@
-const Payment = require("../models/Payment");
+const Payment =
+require("../models/Payment");
 
-exports.submitPayment = async (
-  req,
-  res
-) => {
-  try {
-    const {
-      orderId,
-      paymentMethodId,
-      senderNumber,
-      transactionId,
-      amount
-    } = req.body;
+exports.submitPayment =
+async (req,res)=>{
+
+  try{
+
+    const exists =
+    await Payment.findOne({
+      where:{
+        transactionId:
+        req.body.transactionId
+      }
+    });
+
+    if(exists){
+
+      return res.status(400).json({
+        success:false,
+        message:
+        "Transaction ID already used"
+      });
+
+    }
 
     const payment =
-      await Payment.create({
-        orderId,
-        paymentMethodId,
-        senderNumber,
-        transactionId,
-        amount,
-        screenshot:
-          req.file?.filename || null
-      });
+    await Payment.create({
+
+      ...req.body,
+
+      screenshot:
+      req.file?.filename
+
+    });
 
     res.status(201).json({
-      success: true,
-      data: payment
+      success:true,
+      data:payment
     });
-  } catch (error) {
+
+  }catch(error){
+
     res.status(500).json({
-      success: false,
-      message: error.message
+      success:false,
+      message:error.message
     });
+
   }
 };
-
-exports.getPayments = async (
-  req,
-  res
-) => {
-  try {
-    const payments =
-      await Payment.findAll({
-        order: [
-          ["createdAt", "DESC"]
-        ]
-      });
-
-    res.json({
-      success: true,
-      data: payments
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    });
-  }
-};
-
-exports.approvePayment =
-  async (req, res) => {
-    try {
-      const payment =
-        await Payment.findByPk(
-          req.params.id
-        );
-
-      if (!payment) {
-        return res.status(404).json({
-          success: false,
-          message:
-            "Payment not found"
-        });
-      }
-
-      payment.status = "approved";
-
-      await payment.save();
-
-      res.json({
-        success: true,
-        message:
-          "Payment approved"
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message
-      });
-    }
-  };
-
-exports.rejectPayment =
-  async (req, res) => {
-    try {
-      const payment =
-        await Payment.findByPk(
-          req.params.id
-        );
-
-      if (!payment) {
-        return res.status(404).json({
-          success: false,
-          message:
-            "Payment not found"
-        });
-      }
-
-      payment.status = "rejected";
-
-      await payment.save();
-
-      res.json({
-        success: true,
-        message:
-          "Payment rejected"
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: error.message
-      });
-    }
-  };
