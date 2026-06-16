3470baa6190fcd8d35884152748289c9323ae358
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 00:46:53 +0600

    Create settings.controller.js

---
+++ backend/src/controllers/settings.controller.js
@@ -0,0 +1,39 @@
+const Settings = require("../models/Settings");
+
+exports.getSettings = async (req, res) => {
+  try {
+    const settings = await Settings.findOne();
+
+    res.json({
+      success: true,
+      data: settings
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+exports.updateSettings = async (req, res) => {
+  try {
+    let settings = await Settings.findOne();
+
+    if (!settings) {
+      settings = await Settings.create(req.body);
+    } else {
+      await settings.update(req.body);
+    }
+
+    res.json({
+      success: true,
+      data: settings
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
