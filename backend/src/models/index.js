798adbf7478ee15c10c5b4c9d7bece766bc5ba9a
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 20:49:02 +0600

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,23 @@
+const User = require("./User");
+const Admin = require("./Admin");
+const App = require("./App");
+const Plan = require("./Plan");
+const Duration = require("./Duration");
+const Order = require("./Order");
+const Payment = require("./Payment");
+const PaymentMethod = require("./PaymentMethod");
+const InventoryAccount = require("./InventoryAccount");
+const Settings = require("./Settings");
+
+module.exports = {
+  User,
+  Admin,
+  App,
+  Plan,
+  Duration,
+  Order,
+  Payment,
+  PaymentMethod,
+  InventoryAccount,
+  Settings
+};
