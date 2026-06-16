3e82e5d7ad6c579c42f32dd778362057de8f3e54
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 20:57:37 +0600

    Update seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO apps(name,slug)
+VALUES
+('ChatGPT','chatgpt'),
+('Gemini','gemini'),
+('Grok','grok'),
+('Veo','veo'),
+('YouTube','youtube'),
+('Spotify','spotify'),
+('CapCut','capcut'),
+('InShot','inshot');
+
+INSERT INTO payment_methods
+(name,accountNumber,accountType,status)
+VALUES
+('bKash','017XXXXXXXX','Personal','active'),
+('Nagad','017XXXXXXXX','Personal','active'),
+('Rocket','017XXXXXXXX','Personal','active'),
+('Upay','017XXXXXXXX','Personal','active'),
+('Cellfin','017XXXXXXXX','Personal','active');
