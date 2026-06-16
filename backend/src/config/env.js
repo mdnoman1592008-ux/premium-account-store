6a715667f7d7610709a061236d47f9be572f3d50
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 20:40:43 +0600

    Update env.js

---
+++ backend/src/config/env.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
+};
