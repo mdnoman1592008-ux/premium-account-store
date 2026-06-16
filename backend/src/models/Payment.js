8626accd5163c185f98078a795e1ed4674ea6c33
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-17 01:19:34 +0600

    Update Payment.js

--- backend/src/models/Payment.js
+++ backend/src/models/Payment.js
@@ -1,56 +1,13 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../config/db");
+transactionId:{
+  type:DataTypes.STRING,
+  unique:true,
+  allowNull:false
+},
 
-const Payment = sequelize.define(
-  "Payment",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
-    },
+verifiedBy:{
+  type:DataTypes.INTEGER
+},
 
-    orderId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    paymentMethodId: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-
-    senderNumber: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    transactionId: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    screenshot: {
-      type: DataTypes.STRING
-    },
-
-    amount: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false
-    },
-
-    status: {
-      type: DataTypes.ENUM(
-        "pending",
-        "approved",
-        "rejected"
-      ),
-      defaultValue: "pending"
-    }
-  },
-  {
-    timestamps: true
-  }
-);
-
-module.exports = Payment;
+verifiedAt:{
+  type:DataTypes.DATE
+}
