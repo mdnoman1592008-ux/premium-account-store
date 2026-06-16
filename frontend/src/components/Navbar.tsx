da42840a018ba256a5985ac012dab271ea7de371
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 21:00:29 +0600

    Update Navbar.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        Premium Account Store
+      </div>
+
+      <ul>
+
+        <li>Home</li>
+
+        <li>Store</li>
+
+        <li>My Orders</li>
+
+        <li>Contact</li>
+
+        <li>Login</li>
+
+      </ul>
+
+    </nav>
+  );
+}
