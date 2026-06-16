9730e65630354f5cf1fc0734bf16417d0a6cf64c
mdnoman1592008-ux <mdnoman1592008@gmail.com>
2026-06-16 21:39:28 +0600

    Create page.tsx

---
+++ frontend/src/app/terms/page.tsx
@@ -0,0 +1,48 @@
+export default function TermsPage() {
+
+  return (
+    <div
+      style={{
+        padding:"50px",
+        maxWidth:"900px",
+        margin:"auto"
+      }}
+    >
+
+      <h1>
+        Terms & Conditions
+      </h1>
+
+      <p
+        style={{
+          marginTop:"20px"
+        }}
+      >
+        By using Premium
+        Account Store you
+        agree to our terms.
+      </p>
+
+      <p>
+        Orders will only be
+        delivered after
+        successful payment
+        verification.
+      </p>
+
+      <p>
+        Incorrect transaction
+        information may cause
+        delays or rejection.
+      </p>
+
+      <p>
+        Users are responsible
+        for maintaining the
+        security of delivered
+        accounts.
+      </p>
+
+    </div>
+  );
+}
